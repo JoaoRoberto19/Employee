0c93571739285cd85db72650423aa7b0a934e82d
JoaoRoberto19 <mouravieiraj@gmail.com>
2024-06-25 16:15:16 -0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,52 @@
+package Entities2;
+
+public class Employee {
+
+	private String name;
+	protected Integer hours;
+	protected Double valuePerHour;
+	
+	
+	public Employee () {};
+	
+	
+	public Employee(String name, Integer hour, Double valuePerHour) {
+		this.name = name;
+		this.hours = hour;
+		this.valuePerHour = valuePerHour;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Integer getHour() {
+		return hours;
+	}
+
+
+	public void setHour(Integer hour) {
+		this.hours = hour;
+	}
+
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public double payment() {
+		return hours * valuePerHour;
+	}
+}
